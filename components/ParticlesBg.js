f887cf50570601eb588c507f06a83240fb24cf32
cainiao-7777 <zhaoqi775@proton.me>
2025-05-17 16:25:28 +0800

    Create ParticlesBg.js

---
+++ components/ParticlesBg.js
@@ -0,0 +1,39 @@
+import React from 'react'
+import Particles from 'react-tsparticles'
+import { loadFull } from 'tsparticles'
+
+export default function ParticlesBg() {
+  const particlesInit = async (main) => {
+    await loadFull(main)
+  }
+
+  return (
+    <Particles
+      id="tsparticles"
+      init={particlesInit}
+      options={{
+        fullScreen: { enable: true, zIndex: -1 },
+        background: { color: '#000000' },
+        particles: {
+          number: { value: 60 },
+          shape: {
+            type: 'image',
+            image: {
+              src: 'https://cdn.jsdelivr.net/gh/stevenjoezhang/live2d-widget@latest/heart.png',
+              width: 100,
+              height: 100
+            }
+          },
+          size: { value: 16 },
+          opacity: { value: 0.6 },
+          move: {
+            enable: true,
+            speed: 1.5,
+            direction: 'top',
+            outModes: 'out'
+          }
+        }
+      }}
+    />
+  )
+}
